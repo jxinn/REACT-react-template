27db486d4450ac8bda8f2b283534182f5b496858
jxinn <skshee3@naver.com>
2022-12-12 22:57:23 +0900

    add. Login react-hook-form

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,32 +1,85 @@
 import styled from "styled-components";
 import { VerticalWrap } from "@Components/Layout";
-import { VerticalForm } from "@Components/Form";
+import { FormError, VerticalForm } from "@Components/Form";
 import InputWrap from "@Components/Input";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
+interface ILoginForm {
+  email: string;
+  password: string;
+}
 export default function Login() {
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
+
+  const onCompleted = () => {
+    const { email, password } = getValues();
+    // TODO: EXECUTE REACT QUERY.
+    console.log(getValues());
+    alert(isValid);
+  };
+
   return (
     <>
       <VerticalWrap />
-      <VerticalForm>
-        <LoginFormBox>
-          <LoginTitle>Login</LoginTitle>
-          <LoginInput>
-            <InputWrap>
-              <input className="inp" placeholder="Email" />
-            </InputWrap>
-            <InputWrap>
-              <input className="inp" placeholder="Password" />
-            </InputWrap>
-          </LoginInput>
-        </LoginFormBox>
-        <LoginBtnBox>
-          <LoginBtn as="button">Login</LoginBtn>
-          <LoginBtn to="/signup" bgColor="#EFF2F1" color="#2F2F2F">
-            SignUp
-          </LoginBtn>
-        </LoginBtnBox>
-      </VerticalForm>
+      <form onSubmit={handleSubmit(onCompleted)}>
+        <VerticalForm>
+          <LoginFormBox>
+            <LoginTitle>Login</LoginTitle>
+            <LoginInput>
+              <InputWrap>
+                <input
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value:
+                        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      message: "Please enter a valid email",
+                    },
+                  })}
+                  className="inp"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="off"
+                />
+              </InputWrap>
+              {errors.email?.message && (
+                <FormError message={errors.email.message} />
+              )}
+              <InputWrap>
+                <input
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 10,
+                      message: "Password must be more than 10 chars.",
+                    },
+                  })}
+                  className="inp"
+                  type="password"
+                  placeholder="Password"
+                />
+              </InputWrap>
+              {errors.password?.message && (
+                <FormError message={errors.password.message} />
+              )}
+            </LoginInput>
+          </LoginFormBox>
+          <LoginBtnBox>
+            <LoginBtn as="button">Login</LoginBtn>
+            <LoginBtn to="/signup" bgcolor="#EFF2F1" color="#2F2F2F">
+              SignUp
+            </LoginBtn>
+          </LoginBtnBox>
+        </VerticalForm>
+      </form>
     </>
   );
 }
@@ -46,13 +99,13 @@ const LoginTitle = styled.h1`
 const LoginInput = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
 `;
 const LoginBtnBox = styled.div`
   display: flex;
   height: 50px;
 `;
-const LoginBtn = styled(Link)<{ bgColor?: string; color?: string }>`
+const LoginBtn = styled(Link)<{ bgcolor?: string; color?: string }>`
   flex: 1;
   display: flex;
   align-items: center;
@@ -61,6 +114,6 @@ const LoginBtn = styled(Link)<{ bgColor?: string; color?: string }>`
   border: none;
   font-size: 14px;
   font-weight: 400;
-  background-color: ${(props) => props.bgColor || "#2F2F2F"};
+  background-color: ${(props) => props.bgcolor || "#2F2F2F"};
   color: ${(props) => props.color || "#FFFFFF"};
 `;
